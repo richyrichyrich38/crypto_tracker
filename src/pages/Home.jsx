c448bfaf782a4d62d64adddfd4482dfd0486ac59
richyrichyrich38 <85546279+richyrichyrich38@users.noreply.github.com>
2023-02-15 16:57:42 +0000

    added trrnding api now adding search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import homeStore from '../stores/homeStore'
+import { Link } from 'react-router-dom'
 
-export default function Home() {
+function Home() {
+  const store = homeStore()
+
+  React.useEffect(() => {
+    store.fetchCoins()
+  }, [])
+
   return (
-    <div>Home</div>
+    <>
+      <input type='text' value={store.query} onChange={store.setQuery}/>
+      {store.coins.map(coin => {
+        return (
+          <div key={coin.id}>
+            <Link to={`/${coin.id}`}>
+              {coin.name}
+            </Link>
+          </div>
+        )
+      })}
+    </>
   )
 }
+
+export default Home
--- src/stores/homeStore.js
+++ src/stores/homeStore.js
@@ -2,8 +2,27 @@ import axios from 'axios';
 import { create } from 'zustand'
 
 const homeStore = create((set) => ({
-  fetchCoins: () => {
-    axios.get('')
+  coins: [],
+  query: '',
+
+  setQuery: (event) => {
+    set({query: event.target.value})
+  },
+
+  fetchCoins: async () => {
+    const res = await axios.get('https://api.coingecko.com/api/v3/search/trending')
+
+    const coins = res.data.coins.map(coin => {
+      return {
+        name: coin.item.name,
+        image: coin.item.large,
+        id: coin.item.id,
+        priceBtc: coin.item.price_btc,
+        symbol: coin.item.symbol,
+      }
+    })
+    set({coins: coins});
+    console.log(coins);
   }
 }))
 
