27bff2a581208707d3c55af162eebfbf70a2a0e1
richyrichyrich38 <85546279+richyrichyrich38@users.noreply.github.com>
2023-02-16 14:34:59 +0000

    chart changed

--- src/stores/showStore.js
+++ src/stores/showStore.js
@@ -1,10 +1,23 @@
 import axios from 'axios';
 import { create } from 'zustand'
 
-const showStore = create((set) => ({
-  fetchData: async (id) => {
-    const res = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=7`);
 
+const showStore = create((set) => ({
+  graphData: [],
+
+  fetchData: async (id) => {
+    const res = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=90`);
+
+    const graphData = res.data.prices.map(price => {
+      const [timeStamp, p] = price;
+      const date = new Date(timeStamp).toLocaleDateString('en-us')
+      return {
+        Date: date,
+        Price: p,
+      };
+    })
+
+    set({graphData: graphData})
     console.log(res.data);
   }
 }))
