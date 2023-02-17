84e58fb88892b612027bb334443d9c001260af83
Andreea Ghenea <andreeag@Andreeas-MBP.lan>
2023-02-17 13:23:42 +0000

    added landing page

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,45 @@
+import './landing.css'
+import React, { useState, useEffect } from 'react';
+// import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row'
+
+import Illustration from '../landing-illustration.png';
+
+
+function Landing() {
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth <= 760);
+        };
+
+        // add an event listener to update the state when the window is resized
+        window.addEventListener('resize', handleResize);
+
+        // remove the event listener on component
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return (
+        <>
+            <Row id='landing' className={`d-flex align-items-center justify-content-evenly ${isSmallScreen ? 'flex-wrap' : ''}`}>
+                <Col lg={7} md={6} className={`landing-content ${isSmallScreen ? 'order-last' : ''}`}>
+                    <h1>Unlock the World of <span className="text-gradient">Cryptocurrencies</span></h1>
+                    <p>Welcome to CryptoVault, your one-stop destination for everything related to cryptocurrencies.You can track the performance of various cryptocurrencies, including Bitcoin, Ethereum, and others, using our real-time charts and indicators.</p>
+                    <form>
+                        <input type="text" id="search" name="search" placeholder="Search 12,000+ tokens..." />
+                        {/* <button type="submit">Go</button> */}
+                    </form>
+                </Col>
+                <Col lg={5} md={6} className={`d-flex justify-content-center ${isSmallScreen ? 'order-first' : ''}`}>
+                    <img src={Illustration} className='landing-img' alt='Crypto world floating up and down' />
+                </Col>
+
+            </Row>
+        </>
+    )
+}
+
+export default Landing;
